a2ff92c5417dbcfbd82631c317ab43064d44187b
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2023-12-21 02:36:23 +0000

    updated

--- Airflow_Basic/Branch/Branch_Example.py
+++ Airflow_Basic/Branch/Branch_Example.py
@@ -1,6 +1,16 @@
 from airflow import DAG
-from airflow.operators.dummay_operator import DummyOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+
+
+def notification_email(**kwargs):
+    if kwargs['ti'].xcom_pull(task_ids='check_data'):
+        return 'send_email_task'
+    else:
+        return 'dummy_task'
+    
+
+        
 
 
