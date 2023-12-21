08f4ea4cecacf0e516c90cd015fc4348f50fca93
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2023-12-21 03:03:13 +0000

    added

--- Airflow_Basic/Branch/Branch_Example.py
+++ Airflow_Basic/Branch/Branch_Example.py
@@ -12,5 +12,43 @@ def notification_email(**kwargs):
     
 
         
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+dag = DAG(
+    'branch_example',
+    default_args=default_args,
+    schedule_interval=None,
+)
+
+start_task = DummyOperator(task_id='start_task', dag=dag)
+
+
+check_data = PythonOperator(
+    task_id='check_data',
+    python_callable=lambda: True,
+    dag=dag,
+)
+
+branch_task = PythonOperator(
+    task_id='branch_task',
+    python_callable=notification_email,
+    provide_context=True,
+    dag=dag,
+)
+
+send_email_task = EmailOperator(
+    task_id='send_email_task',
+    to ='example@example.com',
+    subject='Airflow Alert',
+    html_content=""" <h3>Email Test</h3> """,
+    dag=dag,
+)
+
+dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+
+start_task >> check_data >> branch_task >> [send_email_task, dummy_task]
 
 
