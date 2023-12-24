386f41e38003fd6604cf8de875179ab51e2c4764
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2023-12-24 18:49:40 -0500

    Update xcom_pull.py

--- Airflow_Basic/Branch/xcom_pull.py
+++ Airflow_Basic/Branch/xcom_pull.py
@@ -1,10 +1,8 @@
-from datetime import datetime
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.utils.dates import days_ago
 from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
 from airflow.utils.trigger_rule import TriggerRule
-
 
 def push_function(**kwargs):
     emp = ['Andrew', 'Joel', 'Mark']
@@ -23,27 +21,21 @@ dag = DAG(
 
 start_task = DummyOperator(task_id='start_task', dag=dag)
 
-
 push_task = PythonOperator(
-    task_id='push_task', 
+    task_id='push_task',
     python_callable=push_function,
-    provide_context=True,
-    dag=DAG)
+    dag=dag)
 
 def pull_function(**kwargs):
     emp_name = kwargs['ti'].xcom_pull(task_ids='push_task')
     print(emp_name)
 
 pull_task = PythonOperator(
-    task_id='pull_task', 
+    task_id='pull_task',
     python_callable=pull_function,
-    provide_context=True,
-    dag=DAG)
-
+    dag=dag)
 
 dummy_task_final = DummyOperator(task_id='dummy_task_final', trigger_rule=TriggerRule.ONE_SUCCESS, dag=dag)
 
+start_task >> push_task >> pull_task >> dummy_task_final
 
-start_task>>push_task >> pull_task>>dummy_task_final
-
-push_task >> pull_task
