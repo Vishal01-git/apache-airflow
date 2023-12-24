adfbcc836e5a2c3caf270b5949874192d464e02e
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2023-12-24 22:12:44 +0000

    updated

--- Airflow_Basic/Back_Fill/bq_partition_bf.py
+++ Airflow_Basic/Back_Fill/bq_partition_bf.py
@@ -1,4 +1,23 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.dummy import DummyOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+dag = DAG{
+    'Back fill example with partitioned bq table',
+    default_args=default_args,
+    schedule_interval=None,
+}
+
+
+start_task = DummyOperator(task_id='start_task', dag=dag)
+
 
 bq_query_task = BigQueryOperator(
     task_id='run_bq_query',
@@ -7,5 +26,9 @@ bq_query_task = BigQueryOperator(
     destination_dataset_table='your_dataset.destination_table',
     write_disposition='WRITE_TRUNCATE',
     create_disposition='CREATE_IF_NEEDED',
-    dag=your_dag,
+    dag=dag,
 )
+
+final_task = DummyOperator(task_id='final_task', dag=dag)
+
+start_task >> bq_query_task >> final_task
